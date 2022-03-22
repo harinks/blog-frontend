555ffacb3b816262c55b8af3f049c95211468f3b
nkshariharan <nkshariharan17@gmail.com>
2022-03-22 13:34:11 +0530

    slice setup - redux

---
+++ src/features/userSlice.js
@@ -0,0 +1,22 @@
+import { createSlice } from '@reduxjs/toolkit'
+import appApi from '../services/appApi';
+
+const initialState = {}
+
+export const userSlice = createSlice({
+    name: 'user',
+    initialState,
+    extraReducers: (builder) => {
+        builder.addMatcher(appApi.endpoints.loginUser.matchFulfilled, (state, { payload }) => {
+            state.user = payload.user;
+            state.token = payload.token;
+        });
+
+        builder.addMatcher(appApi.endpoints.logoutUser.matchFulfilled, (state) => {
+            delete state.user;
+            delete state.token;
+        });
+    }
+})
+
+export default userSlice.reducer;
